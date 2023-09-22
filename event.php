c9cb7b0a57e8beef0e8fc8b23eda968c971d2126
Seryxss <patrick.alexander1121@gmail.com>
2023-09-22 21:11:14 +0700

    tambah data

--- event.php
+++ event.php
@@ -14,7 +14,7 @@ require_once('./conn.php');
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Dashboard</title>
+    <title>SB Admin 2 - Event</title>
 
     <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
