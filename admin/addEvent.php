2094f664d60f6843d3db1cf53ef23a07086f5b15
Seryxss <patrick.alexander1121@gmail.com>
2023-09-24 06:16:03 +0700

    bug fixes

--- admin/addEvent.php
+++ admin/addEvent.php
@@ -281,7 +281,7 @@ function getName($n = 10)
                 <footer class="sticky-footer bg-white">
                     <div class="container my-auto">
                         <div class="copyright text-center my-auto">
-                            <span>Copyright &copy; Your Website 2021</span>
+                            <span>Copyright &copy; Your Website 2023</span>
                         </div>
                     </div>
                 </footer>
--- admin/detail-varian.php
+++ admin/detail-varian.php
@@ -298,7 +298,7 @@ $queryVarian = mysqli_fetch_array($Varian);
                 <footer class="sticky-footer bg-white">
                     <div class="container my-auto">
                         <div class="copyright text-center my-auto">
-                            <span>Copyright &copy; Your Website 2021</span>
+                            <span>Copyright &copy; Your Website 2023</span>
                         </div>
                     </div>
                 </footer>
